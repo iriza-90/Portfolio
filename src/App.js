fd4611d0a2e58fd6b1903c1247431d71944c350c
Joella69 <irizajoella2006@gmail.com>
2024-08-22 20:37:26 +0200

    'updating'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './components/Header.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Services from './components/Services';
 import Projects from './components/Projects';
@@ -7,20 +8,31 @@ import Stats from './components/Stats';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 import Design from './components/Design';
+import ContactPage from './ContactPage';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <HeroSection />
-      <Design />
-      <Services />
-      <Projects />
-      <Stats />
-      <Testimonials />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <Design />
+              <Services />
+              <Projects />
+              <Stats />
+              <Testimonials />
+               <Footer />
+            </>
+          } />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+       
+      </div>
+    </Router>
   );
 };
 
