f77c5019d223bca48e76cc88e80afcb22221779d
Joella69 <irizajoella2006@gmail.com>
2024-08-28 22:51:48 +0200

    'update'

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-// server.js
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// require('dotenv').config(); // For environment variables
+require('dotenv').config(); // For environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Handle cross-origin requests
-app.use(bodyParser.json()); // Parse JSON bodies
+app.use(cors()); 
+app.use(bodyParser.json()); 
 
-// Route to handle contact form submission
+
 app.post('/api/contact', async (req, res) => {
     const { name, email, message } = req.body;
 
-    // Create a transporter object using SMTP with Gmail
+    
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             // type : 'OAuth2',
-            user: 'irizajoella2006@gmail.com', // Your email address
-            pass: 'ihms@J1976', // Your email password
+            user: process.env.EMAIL_USER, 
+            pass: process.env.EMAIL_PASS, 
         },
     });
 
+
+
+
     // Mail options
     let mailOptions = {
-        from: `"${name}" <${email}>`, // The user's email will appear as the sender
-        to: 'irizajoella2006@gmail.com', // Your email (where you receive the message)
+        from: `"${name}" <${email}>`, 
+        to: process.env.EMAIL_USER, 
         subject: `Contact Form Submission from ${name}`,
         text: `You have received a new message from your contact form.\n\nName: ${name}\nEmail: ${email}\nMessage:\n${message}`,
     };
@@ -44,7 +46,10 @@ app.post('/api/contact', async (req, res) => {
     }
 });
 
-// Start server
+console.log('Email User:', process.env.EMAIL_USER);
+console.log('Email Pass:', process.env.EMAIL_PASS);
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
